be5719533e103f31a452ee4ca39e85257dce864d
staykeeter <staykeeter@gmail.com>
2026-04-06 16:11:10 -0600

    Update config_flow.py

--- custom_components/nhl_goal_lights/config_flow.py
+++ custom_components/nhl_goal_lights/config_flow.py
@@ -1,40 +1,23 @@
-from homeassistant import config_entries
-from homeassistant.helpers import selector
 import voluptuous as vol
-from .const import DOMAIN
+from homeassistant import config_entries
+from homeassistant.helpers import config_validation as cv
 
-NHL_TEAMS = [
-    "ANA","ARI","BOS","BUF","CGY","CAR","CHI","COL","CBJ","DAL",
-    "DET","EDM","FLA","LAK","MIN","MTL","NSH","NJD","NYI","NYR",
-    "OTT","PHI","PIT","SJS","SEA","STL","TBL","TOR","VAN","VGK","WSH"
-]
+from .const import DOMAIN, NHL_TEAMS
 
-class NHLGoalLightsFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+class NHLGoalLightsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Config flow for NHL Goal Lights."""
 
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
         if user_input is not None:
-            return self.async_create_entry(
-                title="NHL Goal Lights",
-                data=user_input
-            )
+            return self.async_create_entry(title="NHL Goal Lights", data=user_input)
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({
-                vol.Optional("monitor_teams"): selector.SelectSelector(
-                    selector.SelectSelectorConfig(
-                        options=NHL_TEAMS,
-                        multiple=True,
-                    )
-                ),
-                vol.Optional("all_games", default=True): selector.BooleanSelector(),
-                vol.Optional("wled_devices"): selector.EntitySelector(
-                    selector.EntitySelectorConfig(
-                        domain="light",
-                        multiple=True
-                    )
-                ),
-            }),
-        )
+        data_schema = vol.Schema({
+            vol.Optional("monitor_teams", default=[]): cv.multi_select({t: t for t in NHL_TEAMS}),
+            vol.Optional("all_games", default=True): bool,
+            vol.Optional("wled_devices", default=[]): cv.multi_select({f"WLED-{i}": f"WLED-{i}" for i in range(1, 5)}),
+        })
+
+        return self.async_show_form(step_id="user", data_schema=data_schema)
