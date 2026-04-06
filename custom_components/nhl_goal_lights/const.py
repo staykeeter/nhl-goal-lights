5220e5f3f64649ba9807b073b70faf00c1906b76
staykeeter <staykeeter@gmail.com>
2026-04-06 16:35:30 -0600

    Update const.py

--- custom_components/nhl_goal_lights/const.py
+++ custom_components/nhl_goal_lights/const.py
@@ -1,42 +1,15 @@
 DOMAIN = "nhl_goal_lights"
+DEFAULT_WLED_EFFECT = "Rainbow"
+DEFAULT_WLED_BRIGHTNESS = 200
+DEFAULT_WLED_SPEED = 128
 
-# NHL team abbreviations
-NHL_TEAMS = [
-    "BOS", "NYR", "TOR", "CHI", "MTL", "DET", "FLA", "TBL", "PIT", "WSH",
-    "CBJ", "VGK", "COL", "STL", "MIN", "CAR", "BUF", "NJ", "NYI", "LAK",
-    "SJS", "DAL", "ARI", "EDM", "CGY", "VAN", "SEA", "PHI", "WPG", "OTT"
-]
-
-# Official team colors for WLED
+# Team colors (RGB)
 TEAM_COLORS = {
-    "BOS": [255, 200, 0],
-    "NYR": [0, 0, 255],
     "TOR": [0, 32, 91],
-    "CHI": [206, 17, 38],
-    "MTL": [192, 30, 45],
-    "DET": [200, 16, 46],
-    "FLA": [0, 50, 125],
-    "TBL": [0, 35, 135],
-    "PIT": [255, 200, 0],
-    "WSH": [200, 16, 46],
-    "CBJ": [0, 43, 92],
-    "VGK": [179, 159, 120],
-    "COL": [128, 0, 64],
-    "STL": [0, 42, 92],
-    "MIN": [0, 41, 71],
-    "CAR": [206, 17, 38],
-    "BUF": [0, 35, 135],
-    "NJ": [200, 16, 46],
-    "NYI": [0, 83, 155],
-    "LAK": [0, 0, 0],
-    "SJS": [0, 105, 128],
-    "DAL": [0, 93, 66],
-    "ARI": [153, 0, 0],
-    "EDM": [0, 50, 135],
-    "CGY": [200, 16, 46],
-    "VAN": [0, 31, 91],
-    "SEA": [0, 61, 68],
-    "PHI": [0, 0, 0],
-    "WPG": [0, 35, 135],
-    "OTT": [206, 17, 38]
+    "MTL": [208, 0, 96],
+    "BOS": [0, 0, 0],
+    "NYR": [0, 50, 167],
+    # ... Add all 32 NHL teams
 }
+
+NHL_TEAMS = list(TEAM_COLORS.keys())
