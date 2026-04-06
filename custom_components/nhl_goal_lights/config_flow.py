5e347cd346b99019f6ec32b0b81e9b4b5143bf64
staykeeter <staykeeter@gmail.com>
2026-04-06 16:36:01 -0600

    Update config_flow.py

--- custom_components/nhl_goal_lights/config_flow.py
+++ custom_components/nhl_goal_lights/config_flow.py
@@ -1,23 +1,18 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.helpers import config_validation as cv
-
 from .const import DOMAIN, NHL_TEAMS
 
-class NHLGoalLightsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Config flow for NHL Goal Lights."""
-
+class NHLGoalLightsFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
         if user_input is not None:
             return self.async_create_entry(title="NHL Goal Lights", data=user_input)
 
-        data_schema = vol.Schema({
-            vol.Optional("monitor_teams", default=[]): cv.multi_select({t: t for t in NHL_TEAMS}),
-            vol.Optional("all_games", default=True): bool,
-            vol.Optional("wled_devices", default=[]): cv.multi_select({f"WLED-{i}": f"WLED-{i}" for i in range(1, 5)}),
+        schema = vol.Schema({
+            vol.Optional("monitor_teams", default=[]): vol.All(
+                vol.List(vol.In(NHL_TEAMS))
+            ),
+            vol.Optional("all_games", default=False): bool
         })
-
-        return self.async_show_form(step_id="user", data_schema=data_schema)
+        return self.async_show_form(step_id="user", data_schema=schema)
