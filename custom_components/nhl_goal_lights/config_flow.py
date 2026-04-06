3f79d8ce1cbae958ed552bb1c31eca87cc537070
staykeeter <staykeeter@gmail.com>
2026-04-06 14:25:52 -0600

    Update config_flow.py

--- custom_components/nhl_goal_lights/config_flow.py
+++ custom_components/nhl_goal_lights/config_flow.py
@@ -1,12 +1,35 @@
 from homeassistant import config_entries
+import voluptuous as vol
 from .const import DOMAIN
 
+# NHL team abbreviations
+NHL_TEAMS = [
+    "ANA","ARI","BOS","BUF","CGY","CAR","CHI","COL","CBJ","DAL",
+    "DET","EDM","FLA","LAK","MIN","MTL","NSH","NJD","NYI","NYR",
+    "OTT","PHI","PIT","SJS","SEA","STL","TBL","TOR","VAN","VGK","WSH"
+]
+
 class NHLGoalLightsFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
-    """Minimal config flow for NHL Goal Lights."""
+    """Config flow for NHL Goal Lights."""
 
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        errors = {}
         if user_input is not None:
-            return self.async_create_entry(title="NHL Goal Lights", data={})
-        return self.async_show_form(step_id="user", data_schema={})
+            return self.async_create_entry(
+                title="NHL Goal Lights",
+                data={
+                    "monitor_teams": user_input.get("monitor_teams", []),
+                    "all_games": user_input.get("all_games", True),
+                    "wled_devices": user_input.get("wled_devices", [])
+                }
+            )
+
+        schema = vol.Schema({
+            vol.Optional("monitor_teams", default=[]): vol.MultiSelect({t: t for t in NHL_TEAMS}),
+            vol.Optional("all_games", default=True): bool,
+            vol.Optional("wled_devices", default=[]): vol.Schema([str])
+        })
+
+        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
