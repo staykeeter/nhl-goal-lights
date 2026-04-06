3902151946cb26f6a63b16f3e9ed13d7a156da4d
staykeeter <staykeeter@gmail.com>
2026-04-06 16:07:35 -0600

    Update const.py

--- custom_components/nhl_goal_lights/const.py
+++ custom_components/nhl_goal_lights/const.py
@@ -2,3 +2,10 @@ DOMAIN = "nhl_goal_lights"
 API_URL = "https://api-web.nhle.com/v1/score/now"
 SCAN_INTERVAL = 10
 STORE_KEY = "nhl_goal_lights.devices"
+TEAM_COLORS = {
+    "BOS": [255, 200, 0],
+    "NYR": [0, 0, 255],
+    "TOR": [0, 32, 91],
+    "CHI": [206, 17, 38],
+    # Add all NHL teams here
+}
